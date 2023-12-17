11d2565d21907e52d08349b7f2da113159b50e4c
takuzo <takuya.koiso.s@gmail.com>
2023-12-17 14:30:40 +0900

    add validate spec to model project

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,31 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Project, type: :model do
-  it "does not allow duplicate project names per user" do
-    user = User.create(
-      first_name: "Aron", 
-      last_name: "Summer", 
-      email: "tester@example.com", 
-      password: "password")
-    user.projects.create(name: "Test Project")
-    new_project = user.projects.build(name: "Test Project")
-    new_project.valid?
-    expect(new_project.errors[:name]).to include("has already been taken")
+  it 'is invalid without a name' do
+    project = Project.new(name: nil)
+    project.valid?
+    expect(project.errors[:name]).to include("can't be blank")
   end
-  it "allows two users to share a project name" do
-    user = User.create(
+  describe 'create project' do
+    before do
+      # ユーザー作成
+      @user = User.create(
       first_name: "Aron", 
       last_name: "Summer", 
       email: "tester@example.com", 
       password: "password")
-    user.projects.create(name: "Test Project")
-    other_user = User.create(
+      @user.projects.create(name: "Test Project")
+    end
+    it "does not allow duplicate project names per user" do
+      
+      new_project = @user.projects.build(name: "Test Project")
+      new_project.valid?
+      expect(new_project.errors[:name]).to include("has already been taken")
+    end
+    it "allows two users to share a project name" do
+      other_user = User.create(
       first_name: "Aron2", 
       last_name: "Summer2", 
       email: "tester@example.com2", 
       password: "password2")
-    ohter_project = other_user.projects.build(name:"Test Project")
-    
-    expect(ohter_project).to be_valid
+      ohter_project = other_user.projects.build(name:"Test Project")
+      
+      expect(ohter_project).to be_valid
+    end
   end
 end
+  
