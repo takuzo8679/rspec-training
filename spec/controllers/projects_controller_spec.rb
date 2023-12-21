c1463cd429e0b01b198910c9f804b022673c8fd0
takuzo <takuya.koiso.s@gmail.com>
2023-12-22 08:57:09 +0900

    ユーザー入力のテストを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -11,7 +11,6 @@ RSpec.describe ProjectsController, type: :controller do
         get "index"
         expect(response).to be_successful
       end
-      
       it "returns a 200 response" do
         get :index
         expect(response).to have_http_status "200"
@@ -50,6 +49,33 @@ RSpec.describe ProjectsController, type: :controller do
       it "redirect to the dashboard" do
         get :show, params: { id: @project.id }
         expect(response).to redirect_to root_path
+      end
+    end
+  end
+  describe "#create" do
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+        sign_in @user
+      end
+      it "adds a project" do
+        project_params = FactoryBot.attributes_for(:project)
+        # blockで渡す必要がる
+        expect {
+        post :create, params: { project: project_params }
+      }.to change(@user.projects, :count).by(1)
+      end
+    end
+    context "as a guest" do
+      before do
+        project_params = FactoryBot.attributes_for(:project)
+        post :create, params: {project: project_params}
+      end
+      it "returns a 302 response" do
+        expect(response).to have_http_status(302)
+      end
+      it "redirects to the sign-in page" do
+        expect(response).to redirect_to '/users/sign_in'
       end
     end
   end
