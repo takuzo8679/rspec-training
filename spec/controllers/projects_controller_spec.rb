06a2f6ce685ca85c1ff9112721d737329f4fa582
takuzo <takuya.koiso.s@gmail.com>
2023-12-23 13:03:10 +0900

    destroyのテストを追加

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -109,4 +109,46 @@ RSpec.describe ProjectsController, type: :controller do
       end
     end
   end
+  describe "#destroy" do
+    before do
+      @user = FactoryBot.create(:user)
+      @project = FactoryBot.create(:project, owner: @user)
+    end
+    context "as an authorized user" do
+      it "response successfully" do
+        sign_in @user
+        expect {delete :destroy, params: { id: @project.id }}
+        .to change(@user.projects, :count).by(-1)
+      end
+    end
+    context "as an unauthorized user" do
+      before do
+        other_user = FactoryBot.create(:user)
+        sign_in other_user
+      end
+      it "does not delete the project" do
+        expect {delete :destroy, params: { id: @project.id }}
+          .to_not change(@user.projects, :count)
+      end
+      it "rediredt to root_path" do
+        delete :destroy, params: { id: @project.id }
+        expect(response).to redirect_to root_path
+      end
+    end
+    context "as a guest" do
+      it "return 302 response" do
+        delete :destroy, params: { id: @project.id }
+        expect(response).to have_http_status '302'
+      end
+      it "redirect to sign_in" do
+        delete :destroy, params: { id: @project.id }
+        expect(response).to redirect_to '/users/sign_in'
+      end
+      it "does not delete the project" do
+        expect {delete :destroy, params: { id: @project.id }}
+          .to_not change(@user.projects, :count)
+      end
+
+    end
+  end
 end
