4c4571000af153329faebd91f194949220580b79
takuzo <takuya.koiso.s@gmail.com>
2023-12-24 08:12:18 +0900

    ヘッドレスドライバを使用するように指定とデフォルトの待ち時間を設定

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,9 +1,12 @@
 RSpec.configure do |config|
+  
+  # Capybara.default_max_wait_time = 10
+
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
 
   config.before(:each, type: :system, js: true) do
-    driven_by :selenium_chrome
+    driven_by :selenium_chrome_headless
   end
 end
