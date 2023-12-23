5120bf37e97d0a4bfadd07f9a4459ef76d80cd44
takuzo <takuya.koiso.s@gmail.com>
2023-12-24 06:10:40 +0900

    システムテストを追加

---
+++ spec/system/projects_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.describe "Projects", type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  scenario "user creates a new project" do
+    user = FactoryBot.create(:user)
+
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+
+    expect {
+      click_link "New Project"
+      fill_in "Name", with: "Test project"
+      fill_in "Description", with: "Trying Capybara"
+      click_button "Create Project"
+
+      expect(page).to have_content("Project was successfully created.")
+      expect(page).to have_content("Test project")
+      expect(page).to have_content("Trying Capybara")
+      expect(page).to have_content("Owner: #{user.name}")
+    }.to change(user.projects, :count).by(1)
+  end
+end
