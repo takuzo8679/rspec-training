94cc4aedef358505f9558accccf2522117504def
takuzo <takuya.koiso.s@gmail.com>
2023-12-26 11:46:42 +0900

    task_controller_specをletでリファクタ

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -1,37 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe TasksController, type: :controller do
-  before do
-    @user = FactoryBot.create(:user)
-    @project = FactoryBot.create(:project, owner: @user)
-    @task = @project.tasks.create!(name: "Test task")
-  end
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user) }
+  let(:task) { project.tasks.create!(name: "Test task") }
+
   describe "#show" do
     it "responds with json formatted output" do
-      sign_in @user
+      sign_in user
       get :show, format: :json,
-        params: {project_id: @project.id, id: @task.id }
+        params: {project_id: project.id, id: task.id }
       expect(response.content_type).to include "application/json"
     end
   end
   describe "#create" do
     it "responds with json formatted output" do
-      sign_in @user
+      sign_in user
       post :create, format: :json,
-        params: {project_id: @project.id, task: {name: "Test task 2"} }
+        params: {project_id: project.id, task: {name: "Test task 2"} }
       expect(response.content_type).to include "application/json"
     end
     it "adds a new task" do
-      sign_in @user
+      sign_in user
       expect { post :create, format: :json,
-        params: {project_id: @project.id, task: {name: "Test task 2"}}
-      }.to change(@project.tasks, :count).by(1)
+        params: {project_id: project.id, task: {name: "Test task 2"}}
+      }.to change(project.tasks, :count).by(1)
     end
     it "requires authentification" do
-      # sign_in @user
+      # sign_in user
       expect { post :create, format: :json,
-        params: {project_id: @project.id, task: {name: "Test task 2"}}
-      }.to_not change(@project.tasks, :count)
+        params: {project_id: project.id, task: {name: "Test task 2"}}
+      }.to_not change(project.tasks, :count)
       expect(response).to_not be_successful
     end
   end
