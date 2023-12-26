615d9cfd2f208a3fb8780ebe1e06818d1d922c70
takuzo <takuya.koiso.s@gmail.com>
2023-12-26 12:49:50 +0900

    カスタムマッチャのエラーメッセージを改善

--- spec/support/matchers/content_type.rb
+++ spec/support/matchers/content_type.rb
@@ -1,9 +1,30 @@
 RSpec::Matchers.define :have_content_type do |expected|
   match do |actual|
-    content_types = {
+    begin
+      actual.content_type.include? content_type(expected)
+    rescue ArgumentError
+      false
+    end
+  end
+
+  failure_message do |actual|
+    "Expected \"#{content_type(actual.content_type)}" +
+    "(#{actual.content_type})\" to be Content Type " +
+    "\"#{content_type(expected)}\" (#{expected})"
+  end
+
+  failure_message_when_negated do |actual|
+    "Expected \"#{content_type(actual.content_type)}" +
+    "(#{actual.content_type})\" to not be Content Type " +
+    "\"#{content_type(expected)}\" (#{expected})"
+  end
+
+  def content_type(type)
+    types = {
       html: "text/html",
       json: "application/json",
     }
-    actual.content_type.include? content_types[expected.to_sym]
+    types[type.to_sym] || "unknown content type"
   end
+
 end
