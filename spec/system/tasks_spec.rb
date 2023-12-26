1348c87d05fdc34e1565e4651d1d2361aedf8300
takuzo <takuya.koiso.s@gmail.com>
2023-12-26 13:46:17 +0900

    シングルレベルの抽象化を実施

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,19 +2,37 @@ require 'rails_helper'
 
 RSpec.describe "Tasks", type: :system do
   include LoginSupport
+  # データ生成
+  let(:user) { FactoryBot.create(:user) }
+  let(:project) { FactoryBot.create(:project, owner: user, name: "Scenario Task") }
+  let!(:task) { project.tasks.create!(name: 'Finish Rspec tutorial') }
+
   scenario "user toggle a task", js:true do
-    # データ生成
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(:project, owner: user, name: "Scenario Task")
-    task = project.tasks.create!(name: 'Finish Rspec tutorial')
     sign_in user
-    # テスト画面まで移動
+    go_to_project
+
+    complete_task "Finish Rspec tutorial"
+    expect_complete_task "Finish Rspec tutorial"
+    uncomplete_task "Finish Rspec tutorial"
+    expect_uncomplete_task "Finish Rspec tutorial"
+  end
+
+  def go_to_project
     visit root_path
     click_link "Scenario Task"
-    # テスト
-    check "Finish Rspec tutorial"
-    expect(page).to have_css "label#task_#{task.id}.completed"
-    uncheck "Finish Rspec tutorial"
-    expect(page).to_not have_css "label#task_#{task.id}.completed"
+  end
+  def complete_task(name)
+    check name
+  end
+  def uncomplete_task(name)
+    uncheck name
+  end
+  def expect_complete_task(name)
+    expect(page).to have_css "label.completed", text: name
+    expect(task.reload).to be_completed
+  end
+  def expect_uncomplete_task(name)
+    expect(page).to_not have_css "label.completed", text: name
+    expect(task.reload).to_not be_completed
   end
 end
