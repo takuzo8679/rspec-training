aa8ec82a6dfe22c50daf636c2ddf974e2d466dfe
takuzo <takuya.koiso.s@gmail.com>
2023-12-26 13:04:18 +0900

    aggregate_failuresによる失敗テストの集約

--- spec/system/projects_spec.rb
+++ spec/system/projects_spec.rb
@@ -14,12 +14,14 @@ RSpec.describe "Projects", type: :system do
       fill_in "Name", with: "Test project"
       fill_in "Description", with: "Trying Capybara"
       click_button "Create Project"
-
-      expect(page).to have_content("Project was successfully created.")
-      expect(page).to have_content("Test project")
-      expect(page).to have_content("Trying Capybara")
-      expect(page).to have_content("Owner: #{user.name}")
     }.to change(user.projects, :count).by(1)
+
+      aggregate_failures do
+        expect(page).to have_content("Project was successfully created.")
+        expect(page).to have_content("Test project")
+        expect(page).to have_content("Trying Capybara")
+        expect(page).to have_content("Owner: #{user.name}")
+      end
   end
   # # デバッグ用save_pageの確認用テスト
   # scenario "guest add a project" do
