02b408bf24c5ab27fc036e6cbf8070ae1982a2dc
takuzo <takuya.koiso.s@gmail.com>
2023-12-29 03:43:37 +0900

    databaseにcompletedカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema[7.0].define(version: 2021_12_31_004155) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.boolean "completed"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
