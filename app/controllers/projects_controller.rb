b06c8d4b1025d1e96cbfa21c7c9a07e7112dff09
takuzo <takuya.koiso.s@gmail.com>
2023-12-29 04:11:42 +0900

    controllerにアクションを追加(TDDの信条で最小コード)

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -58,6 +58,9 @@ class ProjectsController < ApplicationController
     end
   end
 
+  def complete
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_project
